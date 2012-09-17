fba7e3d56ffcecc7ac6bd5744cb474aa38dc718c
Chris <cj3kim@gmail.com>
2012-09-17 14:15:57 -0700

    refactored if statements

--- kaldreyr-beginner/player.rb
+++ kaldreyr-beginner/player.rb
@@ -19,26 +19,44 @@ class Player
   end
 
   def actions(warrior)
+    wall?(warrior)
+    rescue_captive(warrior)
+    swing_sword(warrior)
+    shoot_bow(warrior)
+    back_away(warrior)
+    safe_rest(warrior)
+    walk_forward!(warrior)
+  end
 
-    p warrior.look
 
-    if warrior.feel.wall?
-      warrior.pivot!
-    elsif warrior.feel.captive?
-      warrior.rescue!
-    elsif warrior.feel(:backward).captive?
-      warrior.rescue!(:backward)
-    elsif !warrior.feel.empty? and !warrior.feel.captive?
-      return warrior.attack!
-    elsif @damage_since_last_turn and warrior.feel.empty? and warrior.health < 10
-      return warrior.walk!(:backward)
-    elsif !@damage_since_last_turn and warrior.health < 15
-      return warrior.rest!
-    elsif warrior.feel.empty?
-      return warrior.walk!
-    else
-      return
-    end
+  def wall?(warrior)
+    return warrior.pivot! if warrior.feel.wall?
+  end
+
+  def back_away(warrior)
+    return warrior.walk!(:backward) if @damage_since_last_turn and warrior.feel.empty? and warrior.health < 10
+  end
+
+  def shoot_bow(warrior)
+    return warrior.shoot!
+  end
+
+  def rescue_captive(warrior)
+    return warrior.rescue! if warrior.feel.captive?
+    return warrior.rescue!(:backward) if warrior.feel(:backward).captive?
+  end
+
+  def swing_sword(warrior)
+    return warrior.attack! if !warrior.feel.empty? and !warrior.feel.captive?
+    return warrior.attack!(:backward) if !warrior.feel(:backward).empty? and !warrior.feel(:backward).captive?
+  end
+
+  def safe_rest(warrior)
+    return warrior.rest! if !@damage_since_last_turn and warrior.health < 15
+  end
+
+  def walk_forward!(warrior)
+    return warrior.walk! if warrior.feel.empty?
   end
 
   def end_turn_check(warrior)
